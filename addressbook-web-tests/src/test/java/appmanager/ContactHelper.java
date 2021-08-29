4caee96a80c923d9dd25be841a9c5e83e1b02b68
MrGoose <kanes137@gmail.com>
2021-08-29 15:17:35 +0300

    Создал новый класс ContactModificationTests, добавил недостающих методов

--- addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
@@ -32,4 +32,12 @@ public class ContactHelper extends BaseHelper {
   public void deleteSelectedContacts() {
     click(By.xpath("//input[@value='Delete']"));
   }
+
+  public void initContactModification() {
+    click(By.xpath("//img[@alt='Edit']"));
+  }
+
+  public void submitContactModification() {
+    click(By.xpath("//div[@id='content']/form/input[22]"));
+  }
 }
--- addressbook-web-tests/src/test/java/ru/test/homework/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/test/homework/ContactModificationTests.java
@@ -1,11 +1,16 @@
 package ru.test.homework;
 
+import model.ContactData;
 import org.testng.annotations.Test;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
   public void testModificationContact() {
-
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("lol", "Aleks", "Ms", "vaniko"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().goToHomePage();
   }
 }
