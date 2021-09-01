2c166ad5c5df060647c37285822069c9c0fc80be
MrGoose <kanes137@gmail.com>
2021-09-01 18:42:44 +0300

    ДЗ 8. Добавил предусловие создание группы (если она отсутствует) перед создание контакта

--- addressbook-web-tests/src/test/java/ru/test/homework/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/test/homework/ContactCreationTests.java
@@ -1,12 +1,17 @@
 package ru.test.homework;
 
 import model.ContactData;
+import model.GroupData;
 import org.testng.annotations.Test;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "Модификация", "Модификация"));
+    }
     app.getContactHelper().createContact(new ContactData("Firstname", "Middlename", "Lastname", "nickname", "test1"), true);
     app.getNavigationHelper().goToHomePage();
   }
