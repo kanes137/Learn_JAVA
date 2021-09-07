2fef99bfa3143014b1b7cfcf9a9c4873d9682713
MrGoose <kanes137@gmail.com>
2021-09-07 23:28:07 +0300

    Задание №9. Добавил полное сравнение групп до и после теста(+сортировка)

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -4,6 +4,7 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
@@ -15,10 +16,16 @@ public class GroupDeletionTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("Удаление", "Удаление", "Удаление"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after,before);
   }
 }
--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -15,12 +15,14 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("Модификация", "Модификация", "Модификация"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("ДЗ 6", "ДЗ  6", "ДЗ   6"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    Assert.assertEquals(after, before);
   }
 }
