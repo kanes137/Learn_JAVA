6ced2f491bddf4823282c62fe7ae9755fd21bca7
MrGoose <kanes137@gmail.com>
2021-09-07 23:18:38 +0300

    Задание №9. Добавил полное сравнение групп до и после теста

--- addressbook-web-tests/src/test/java/model/GroupData.java
+++ addressbook-web-tests/src/test/java/model/GroupData.java
@@ -4,8 +4,17 @@ public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
+  private int id;
 
   public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -21,5 +30,36 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id=" + id +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
   }
 }
--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -16,5 +17,11 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after,before);
   }
 }
