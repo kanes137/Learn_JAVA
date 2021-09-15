ba1912fb3e64462bc5a535fa0a8f552c6892a711
MrGoose <kanes137@gmail.com>
2021-09-15 23:50:15 +0300

    Добавил получение ID группы по индексу

--- addressbook-web-tests/src/test/java/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactModificationTests.java
@@ -23,7 +23,7 @@ public class ContactModificationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData("Модиф1", "test", "Модиф", "Модиф", null);
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Модиф1", "test", "Модиф", "Модиф", null);
     app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().goHome();
