efbc5880d81de7efbc1281266a674fe51916b892
MrGoose <kanes137@gmail.com>
2021-10-01 21:04:34 +0300

    Задание №14. Добавил возможность указывать тип браузера в конфигурации запуска класса

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -7,7 +7,13 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  /*
+  System.getProperty даёт возможность указывать в конфигурации запуска класса тип запускаемого браузера
+  VM options = -Dbrowser=firefox
+  если в конфигурации ничего не указано, запускается дефолтный - CHROME
+   */
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
